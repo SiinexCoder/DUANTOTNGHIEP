864e4e9e12dbc8577a2120969c915835262565e6
Siinex <117749336+SiinexCoder@users.noreply.github.com>
2024-10-05 00:14:28 +0700

    Quái

--- Assets/Scripts/Monter.cs
+++ Assets/Scripts/Monter.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Monter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform player;
+public float speed = 2.5f;
 
-    // Update is called once per frame
-    void Update()
+public void LookAtPlayer()
+{
+    // Tính toán hướng đến player (chỉ quan tâm đến trục X và Y, không xoay)
+    Vector2 direction = player.position - transform.position;
+
+    // Xoay quái vật để hướng về phía player theo chiều ngang
+    if (direction.x > 0 && transform.localScale.x < 0)
     {
-        
+        // Nếu player ở bên phải và quái đang lật, lật lại
+        transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
     }
+    else if (direction.x < 0 && transform.localScale.x > 0)
+    {
+        // Nếu player ở bên trái và quái không lật, lật lại
+        transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+    }
+}
+
+void Update()
+{
+    // Quái vật di chuyển về phía player
+    Vector2 direction = (player.position - transform.position).normalized;
+    transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+
+    // Gọi hàm LookAtPlayer để đảm bảo quái vật luôn hướng về phía player
+    LookAtPlayer();
+}
+
 }
