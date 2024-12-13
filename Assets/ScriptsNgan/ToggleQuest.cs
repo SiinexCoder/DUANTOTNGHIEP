2138def3a5879b613e76d682a91184c81cab2c9f
Judass2706 <synlun2k3@gmail.com>
2024-12-14 01:35:06 +0700

    up

---
+++ Assets/ScriptsNgan/ToggleQuest.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ToggleQuest : MonoBehaviour
+{
+    public GameObject questPanel; 
+    public GameObject questText;   
+
+
+
+    private bool isQuestVisible = false; 
+
+    // Hàm bật/tắt giao diện balo
+    public void ToggleQuestUI()
+    {
+        isQuestVisible = !isQuestVisible; // Đảo ngược trạng thái
+        questPanel.SetActive(isQuestVisible); // Hiển thị hoặc ẩn panel chứa item
+        questText.SetActive(isQuestVisible);    // Có thể để ẩn nếu không muốn UI phụ xuất hiện
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ToggleQuestUI();
+        }
+    }
+    
+}
